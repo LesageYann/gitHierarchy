9f00c6a4c5b39274382f121c79f56d5a8acc0c76
Bouchouaf Latifa <bouchouaf@b10p13.fil.univ-lille1.fr>
2016-09-27 16:24:40 +0200

    Ajout de la branche server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-initiale le serveur et les différents routeurs
+var express = require('express');
+var app = express();
+
+require("./lib/githubRequest/router.js").init(express,app);
+
+app.listen(3000, function () {
+    console.log('Server run on port 3000');
+});
