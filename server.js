8b41b7abc808d82373a3a334150975c3041e6e6a
valtena <ethanvaltena@gmail.com>
2016-09-28 14:43:25 +0200

    import git ok

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+require("./lib/ui/router.js").init(express,app);
 require("./lib/githubRequest/router.js").init(express,app);
 
 app.listen(3000, function () {
