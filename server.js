f88c8b39edb534883f37433cef3a2e5b8e5517fb
valtena <ethanvaltena@gmail.com>
2016-10-10 23:50:26 +0200

    pre-realese

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-require("./lib/ui/router.js").init(express,app);
-require("./lib/githubRequest/router.js").init(express,app);
-
-app.listen(3000, function () {
-    console.log('Server run on port 3000');
-});
